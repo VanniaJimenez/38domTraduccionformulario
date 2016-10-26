e542625e9386d7fbca64dfae28db31842b469d86
VanniaJimenez <patsymena11@gmail.com>
2016-10-26 08:42:05 -0500

    traducción(etc)

---
+++ js/main.js
@@ -0,0 +1,5 @@
+document.getElementById("form-signin-heading").innerHTML= "Por favor inicia sesión";
+document.getElementById("inputEmail").setAttribute("placeholder","correo electronico");
+document.getElementById("inputPassword").setAttribute("placeholder","contraseña");
+document.getElementsByTagName("span")[0].innerHTML=" Recordar datos";
+document.getElementsByTagName("button")[0].innerHTML="Iniciar sesión";
